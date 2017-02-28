41649790f2f3bf340d9ece139e8c108a18b05a92
englehardt <englehardt@gmail.com>
2017-02-28 12:21:18 -0500

    Remove unused "page manager" from extension

--- automation/Extension/firefox/index.js
+++ automation/Extension/firefox/index.js
@@ -3,7 +3,6 @@ const system            = require("sdk/system");
 const pageMod           = require("sdk/page-mod");
 const data              = require("sdk/self").data;
 var loggingDB           = require("./lib/loggingdb.js");
-var pageManager         = require("./lib/page-manager.js");
 var cookieInstrument    = require("./lib/cookie-instrument.js");
 var jsInstrument        = require("./lib/javascript-instrument.js");
 var cpInstrument        = require("./lib/content-policy-instrument.js");
